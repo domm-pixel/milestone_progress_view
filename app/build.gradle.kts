227b0f8c35fc740c13a135f3a837cf59a0b9d59c
kangoona <kangoona@doldol.net>
2025-09-24 15:59:45 +0900

    feat: library module + publishing config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -8,13 +9,10 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.dom.milestoneprogressview"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // Library: add consumer rules (create this file at module root)
+//        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -25,7 +23,16 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            // no special config
+        }
     }
+
+    buildFeatures {
+        // Library modules usually don't need BuildConfig
+        buildConfig = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -33,13 +40,57 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    // Include sources jar for publishing
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+// Publishing coordinates for JitPack or other Maven repos
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.domm-pixel" // TODO: replace with your GitHub ID
+                artifactId = "milestone-progressview"
+                version = "0.1.0"
+
+                pom {
+                    name.set("Milestone Progress View")
+                    description.set("A customizable milestone-based progress view for Android.")
+                    url.set("https://github.com/domm-pixel/milestone_progress_view")
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/domm-pixel/milestone_progress_view")
+                        connection.set("scm:git:https://github.com/domm-pixel/milestone_progress_view.git")
+                        developerConnection.set("scm:git:ssh://github.com:domm-pixel/milestone_progress_view.git")
+                    }
+                    developers {
+                        developer {
+                            id.set("domm-pixel")
+                            name.set("YOUR_NAME")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
